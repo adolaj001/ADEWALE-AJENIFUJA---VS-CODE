0596d3830bcb1c9919b467e46a831ba670e7b16b
Adewale Ajenifuja <adolaj001@gmail.com>
2026-03-15 16:18:54 +0100

    Update on 15th March 2026

---
+++ Module 4A_Javascript/Task/Task11.js
@@ -0,0 +1,7 @@
+/*
+//Module 4a Task 11
+
+let friends = [];
+friends = ["Olatunde", "Aisha", "Boladale", "Chinedu", "Kemisola"];
+console.log(friends);
+*/
---
+++ Module 4A_Javascript/Task/Task4.js
@@ -0,0 +1,3 @@
+alert("Hello World Testers!");
+const myFavouriteSubject = "Hello World Testers! JavaScript is fun!";
+alert(myFavouriteSubject);
